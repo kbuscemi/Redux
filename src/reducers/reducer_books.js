636b7d11858876502d924b42fe897954476fd35e
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-05-01 17:18:41 -0700

    initial commit with book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+export default function() {
+    //books reducer
+    return [
+        { title: 'JavaScript: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby'}
+    ]
+}
+
+// step 1. write the reducer
+// step 2. wire the reducer to the app
