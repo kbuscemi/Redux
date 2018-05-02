28f9be165471f66e01ec4fa304ea0c02c782f642
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-05-02 14:42:56 -0700

    action creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,8 @@ import BooksReducer from './reducer_books';
 const rootReducer = combineReducers({
   //combineReducers is a mapping of our state
   books: BooksReducer
-
+  // key === books; value === BooksReducer
+  //when passing object (books: BooksReducer) to combineReducers telling Redux how to make our app's state.
 });
 
 export default rootReducer;
